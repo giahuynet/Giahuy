cf16863beab835f6726315491c9ef31802f2f4f1
GiahuyCons™ <87752381+ntgiahuy@users.noreply.github.com>
2022-09-23 14:59:24 +0700

    Delete decode directory

--- decode/js/worker/format.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('message',function(e){var source=e.data.source;try{self._window=self.window;self.window={};self.importScripts('https://cdn.giahuy.net/decode/js/lib/beautify.min.js');source=self.window.js_beautify(source,{unescape_strings:true,jslint_happy:true});self.window=self._window;}catch(err){console.log(err);}
-self.postMessage({result:source,highlight:false});try{self.importScripts('https://cdn.giahuy.net/decode/js/lib/highlight.min.js');source=self.hljs.highlight('javascript',source).value;}catch(err){console.log(err);}
-self.postMessage({result:source,highlight:true});});
